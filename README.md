747511946164bb7dbf95988c1bc5665d0995b6ba
Marcelo Morales <leonardofutcol@gmail.com>
2023-01-06 15:36:56 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,39 @@
+# findSearchTermInBooks
+
+Included in this repository are files to support candidates applying for software engineering roles through the GSA's U.S. Digital Corps program. 
+
+There are three files in this repository we would like you to focus on:
+
+1. This README.
+1. `tester.html`: An HTML file that will run your code.
+1. `book_search.js`: A JavaScript source code file that you should edit for your assessment submission.
+
+## Testing your code
+
+You will need to open the file `tester.html` in a browser to run the code that is contained in `book_search.js`. The output from running that code will appear in the browser's web developer console. Here is documentation for opening the web developer tools for Firefox:
+
+https://firefox-source-docs.mozilla.org/devtools-user/browser_console/index.html#:~:text=You%20can%20open%20the%20Browser,%2B%20J%20on%20a%20Mac
+
+And here are the instructions for Google Chrome:
+
+https://developer.chrome.com/docs/devtools/open/
+
+Either should work; we tested under both on Mac and Linux.
+
+You will submit both the HTML and JavaScript files when you are done. You should not need to edit `tester.html`. We expect all of your work to appear in the file `book_search.js`. 
+
+## Submission
+
+Directions for submission are in the prompt. As a reminder, you should commit your code to a repository called
+
+*firstnamelastname*-usdc-2023
+
+If you were remarkable technologists of years past, you might name your repository
+
+`otisboykin-usdc-2023`
+
+or 
+
+`mariebrown-usdc-2023`
+
+You, of course, should name your repository for the remarkable technologist of civic tech's future that you are.
---
+++ book_search.js
@@ -0,0 +1,104 @@
+/** 
+ * RECOMMENDATION
+ * 
+ * To test your code, you should open "tester.html" in a web browser.
+ * You can then use the "Developer Tools" to see the JavaScript console.
+ * There, you will see the results unit test execution. You are welcome
+ * to run the code any way you like, but this is similar to how we will
+ * run your code submission.
+ * 
+ * The Developer Tools in Chrome are available under the "..." menu, 
+ * futher hidden under the option "More Tools." In Firefox, they are 
+ * under the hamburger (three horizontal lines), also hidden under "More Tools." 
+ */
+
+/**
+ * Searches for matches in scanned text.
+ * @param {string} searchTerm - The word or term we're searching for. 
+ * @param {JSON} scannedTextObj - A JSON object representing the scanned text.
+ * @returns {JSON} - Search results.
+ * */ 
+ function findSearchTermInBooks(searchTerm, scannedTextObj) {
+    /** You will need to implement your search and 
+     * return the appropriate object here. */
+
+    var result = {
+        "SearchTerm": "",
+        "Results": []
+    };
+    
+    return result; 
+}
+
+/** Example input object. */
+const twentyLeaguesIn = [
+    {
+        "Title": "Twenty Thousand Leagues Under the Sea",
+        "ISBN": "9780000528531",
+        "Content": [
+            {
+                "Page": 31,
+                "Line": 8,
+                "Text": "now simply went on by her own momentum.  The dark-"
+            },
+            {
+                "Page": 31,
+                "Line": 9,
+                "Text": "ness was then profound; and however good the Canadian\'s"
+            },
+            {
+                "Page": 31,
+                "Line": 10,
+                "Text": "eyes were, I asked myself how he had managed to see, and"
+            } 
+        ] 
+    }
+]
+    
+/** Example output object */
+const twentyLeaguesOut = {
+    "SearchTerm": "the",
+    "Results": [
+        {
+            "ISBN": "9780000528531",
+            "Page": 31,
+            "Line": 9
+        }
+    ]
+}
+
+/*
+ _   _ _   _ ___ _____   _____ _____ ____ _____ ____  
+| | | | \ | |_ _|_   _| |_   _| ____/ ___|_   _/ ___| 
+| | | |  \| || |  | |     | | |  _| \___ \ | | \___ \ 
+| |_| | |\  || |  | |     | | | |___ ___) || |  ___) |
+ \___/|_| \_|___| |_|     |_| |_____|____/ |_| |____/ 
+                                                      
+ */
+
+/* We have provided two unit tests. They're really just `if` statements that 
+ * output to the console. We've provided two tests as examples, and 
+ * they should pass with a correct implementation of `findSearchTermInBooks`. 
+ * 
+ * Please add your unit tests below.
+ * */
+
+/** We can check that, given a known input, we get a known output. */
+const test1result = findSearchTermInBooks("the", twentyLeaguesIn);
+if (JSON.stringify(twentyLeaguesOut) === JSON.stringify(test1result)) {
+    console.log("PASS: Test 1");
+} else {
+    console.log("FAIL: Test 1");
+    console.log("Expected:", twentyLeaguesOut);
+    console.log("Received:", test1result);
+}
+
+/** We could choose to check that we get the right number of results. */
+const test2result = findSearchTermInBooks("the", twentyLeaguesIn); 
+if (test2result.Results.length == 1) {
+    console.log("PASS: Test 2");
+} else {
+    console.log("FAIL: Test 2");
+    console.log("Expected:", twentyLeaguesOut.Results.length);
+    console.log("Received:", test2result.Results.length);
+}
